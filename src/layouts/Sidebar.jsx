f98b793a85212e3e9a7834213de375bf3ed98439
The_Hari <104263832+JKHari@users.noreply.github.com>
2023-08-03 00:59:44 +0530

    code cleaning and add home design

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,84 +1,20 @@
-// import React from 'react';
-// import './Sidebar.css'; // Create this CSS file to style your sidebar
-
-// const Sidebar = () => {
-//   return (
-//     <div className="sidebar">
-//       <ul>
-//         <li>Home</li>
-//         <li>About</li>
-//         <li>Services</li>
-//         <li>Contact</li>
-//       </ul>
-//     </div>
-//   );
-// };
-// src/Sidebar.js
-// import React from 'react';
+import React from "react";
 // import './Sidebar.css';
-// import { Link } from 'react-router-dom';
-
-// const Sidebar = () => {
-//   const navigateTo = (page) => {
-//     // Implement your navigation logic here
-//     // You can use React Router or any other routing library
-//   };
-
-//   return (
-//     <div className="sidebar">
-//       <ul>
-//         <li onClick={() => navigateTo('home')}>Home</li>
-//         <li onClick={() => navigateTo('about')}>About</li>
-//         <li onClick={() => navigateTo('services')}>Services</li>
-//         <li onClick={() => navigateTo('contact')}>Contact</li>
-//       </ul>
-//     </div>
-//   );
-// };
-
-// const Sidebar = () => {
-//   return (
-//     <div className="sidebar">
-//       <ul>
-//         <li><Link to="/">Home</Link></li>
-//         <li><Link to="/about">About</Link></li>
-//         {/* ... */}
-//       </ul>
-//     </div>
-//   );
-// };
-// export default Sidebar;
-
-// import React from 'react';
-// import './Sidebar.css';
-// import { Link } from 'react-router-dom';
-
-// const Sidebar = () => {
-//   return (
-//     <div className="sidebar">
-//       <ul>
-//         <li><Link to="/">Home</Link></li>
-//         <li><Link to="/about">About</Link></li>
-//         {/* ... */}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
-
-
-// src/Sidebar.js
-import React from 'react';
-import './Sidebar.css';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+    <div className="w-[350px] bg-[#121212] text-start pl-5 py-3">
+      <ul className="text-white pt-5">
+        <li>
+          <div>
+            <img src="" alt="" />
+            <Link to="/">Home</Link>
+          </div>
+        </li>
+        <li>
+          <Link to="/about">Search</Link>
+        </li>
         {/* ... */}
       </ul>
     </div>
@@ -86,8 +22,3 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-
-
-
-
-
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
-// src/pages/home.jsx
-import React from 'react';
+import React from "react";
+// import {image} from '../../public/image.jpg'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className="bg-[#121212] h-screen">
+      <h1 className="text-white py-5 font-bold text-2xl">Enjoy The Music</h1>
+      <div className="bg-[#1e1e1e] w-full px-8 py-10">
+        <div>
+          <div className="w-[200px] h-auto bg-[#121212] p-1 flex flex-col justify-center items-center flex-wrap">
+            <img src="" alt="" className="w-[180px] h-[180px]" />
+            <p className="text-white pt-2">Motivation Song</p>
+            <p className="text-white pt-2">Hip Hop Tamiza</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
