1b8f2787e8e33ef0ed6ba926e92b1476d6df50ac
The_Hari <104263832+JKHari@users.noreply.github.com>
2023-08-23 01:45:08 +0530

    feat(home): play svg bg remove

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div className="w-[350px] bg-black text-start px-5 py-3 ">
-      <ul className="text-white pt-5 ml-2 bg-[#121212] mx-3 rounded-lg flex flex-col justify-center items-start pl-5 py-5">
+      <ul className="text-white pt-5 ml-2 bg-[#121212] w-[220px] fixed  mx-3 rounded-lg flex flex-col justify-center items-start pl-5 py-5">
         <li className="pb-4 ">
           <div className="flex gap-3 justify-center items-center">
             <img src="/search.png" alt="" className="h-5 w-5" />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -138,20 +138,22 @@ const Home = () => {
                   alt="Playing"
                 />
               )}
-              <div className="w-[200px] h-auto bg-[#121212] p-1 flex flex-col justify-center items-center flex-wrap rounded-md ">
+              <div className="w-[200px] h-auto bg-[#121212] p-3 flex flex-col justify-center items-center flex-wrap rounded-md ">
                 <img
                   src={item.img}
                   alt=""
                   className="w-[180px] h-[180px] object-cover"
                 />
-                <p className="text-white pt-2">{item.name}</p>
+                <p className="text-white pt-2 truncate hover:text-clip w-full h-10">
+                  {item.name}
+                </p>
                 <p className="text-white pt-2">{item.author}</p>
               </div>
-              <div className="absolute w-full h-[120px] mt-16 top-2/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100 cursor-pointer bg-black">
+              <div className="w-full  ">
                 <img
                   src="/play.svg"
                   alt=""
-                  className="w-full h-full cursor-pointer"
+                  className="absolute w-[100px] h-[100px] mt-[48px]  top-2/4 left-[60%] transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100 cursor-pointer"
                   onClick={() => handlePlayClick(item.id)}
                 />
                 <audio id={`audio_${item.id}`} className="hidden">
